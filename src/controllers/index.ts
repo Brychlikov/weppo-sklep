d05ca8d0677663a30f5b77361c0f636f36a4f2aa
lubczanska <lupa319@gmail.com>
2022-01-13 19:35:38 +0100

    added simple index, product and product creation views

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,6 +4,11 @@ import { Request, Response } from "express";
  * GET /
  * Home page.
  */
+
 export const index = async (req: Request, res: Response): Promise<void> => {
-    res.render("index", { title: "Express" });
+    const products = [
+        { id: 0, name: "cygan", price: 12, description: "to jest cygan" },
+        { id: 1, name: "bodzio", price: 15, description: "taki o bodź" },
+    ];
+    res.render("index", { products: products });
 };
