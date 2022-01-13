022d399496ae9fc32c9aed3fffb0f072a43c4c2c
brych <brychlikow@gmai.com>
2022-01-13 13:54:50 +0100

    use another template

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,6 @@
-import { Router } from 'express';
-import { getAllUsers, addOneUser, updateOneUser, deleteOneUser } from './Users';
+import { Router } from "express";
+import * as controller from "../controllers/index";
 
+export const index = Router();
 
-// User-route
-const userRouter = Router();
-userRouter.get('/all', getAllUsers);
-userRouter.post('/add', addOneUser);
-userRouter.put('/update', updateOneUser);
-userRouter.delete('/delete/:id', deleteOneUser);
-
-
-// Export the base-router
-const baseRouter = Router();
-baseRouter.use('/users', userRouter);
-export default baseRouter;
+index.get("/", controller.index);
