f672819e808bb1861af35c6e8f7ff44c05e2fc3e
brych <brychlikow@gmai.com>
2022-01-15 20:07:08 +0100

    add some data  plumbing from models to views

--- resetDb.sql
+++ resetDb.sql
@@ -2,11 +2,13 @@ DROP TABLE IF EXISTS products;
 
 CREATE TABLE products (
     id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(50),
-    img_url VARCHAR(100)
+    name VARCHAR(50) NOT NULL,
+    price MONEY NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    img_url VARCHAR(500) NOT NULL
 );
 
-INSERT INTO products (name, img_url)
+INSERT INTO products (name, price, description, img_url)
 VALUES 
-('Kuna', 'https://example.com'), 
-('Jenot', 'https://example.com/2');
+('Kuna', 10, 'ssak drapieżny', 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Martes_martes_crop.jpg'), 
+('Jenot', 20, 'cholera wie co', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Nyctereutes_procyonoides_viverrinus.jpg/1024px-Nyctereutes_procyonoides_viverrinus.jpg');
