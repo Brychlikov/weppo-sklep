5a6462cbcd638d931d179bfa99cc360c80a63482
Achtungus <artur.krzyzynski@gmail.com>
2022-01-27 19:57:15 +0100

    Autoryzacja poczatek

--- resetDb.sql
+++ resetDb.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS products, users;
 
 CREATE TABLE products (
     id SERIAL PRIMARY KEY NOT NULL,
@@ -12,3 +12,10 @@ INSERT INTO products (name, price, description, img_url)
 VALUES 
 ('Kuna', 10, 'ssak drapieżny', 'https://upload.wikimedia.org/wikipedia/commons/f/ff/Martes_martes_crop.jpg'), 
 ('Jenot', 20, 'cholera wie co', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Nyctereutes_procyonoides_viverrinus.jpg/1024px-Nyctereutes_procyonoides_viverrinus.jpg');
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    password VARCHAR(200) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+);
---
+++ src/models/User.ts
@@ -0,0 +1,37 @@
+import { knex } from "../dbConnection";
+
+interface UserI {
+    id: number;
+    name: string;
+    password: string;
+    role: string;
+}
+
+export class User {
+    private id: number;
+    public name: string;
+    public password: string;
+    public role: string;
+
+    private constructor(id: number, name: string, password: string, role: string) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    private static fromI(data: UserI) : User {
+        return new User(data.id, data.name, data.password, data.role);
+    }
+
+    public static async findByName(name: string) : Promise<User | null> {
+        const query = knex<UserI>('users').select("*").where({ name }).first();
+        const p = await query;
+        if(p) {
+            return User.fromI(p);
+        }
+        else {
+            return null;
+        }
+    }
+}
