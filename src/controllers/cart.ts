928df33068ca236edef1526ee9ac3ef87d098f2c
lubczanska <lupa319@gmail.com>
2022-02-02 16:53:51 +0100

    cart and product view

--- src/controllers/cart.ts
+++ src/controllers/cart.ts
@@ -22,11 +22,11 @@ cartRouter.get(
             if (req.signedCookies.cart) {
                 for (var prod_id of req.signedCookies.cart) {
                     var prod = await Product.findById(Number(prod_id));
-                    if(prod) products.push(prod);
+                    if (prod) products.push(prod);
                 }
             }
             // products = await Product.getAll();
-            res.render("index", { products: products }); // user : req.user
+            res.render("cart", { products: products }); // user : req.user
         })();
     },
 );
