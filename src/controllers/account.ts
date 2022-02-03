adae886cec3b07d3e7210a22a67e44791b56d769
lubczanska <lupa319@gmail.com>
2022-02-03 15:00:24 +0100

    fixed cart

--- src/controllers/account.ts
+++ src/controllers/account.ts
@@ -5,13 +5,18 @@ import process from "process";
 import { createAccountRouter } from "./createAccount";
 import { authorize } from "./authorize";
 
-
 export const accountRouter = express.Router();
 accountRouter.use(express.urlencoded({ extended: true }));
 accountRouter.use(cookieParser(process.env.COOKIE_SECRET));
 
-accountRouter.get("/", authorize("Normal", "Admin"), (req: Request, res: Response) => {
-    res.render("acccount.ejs", { url : "/account", cart_item_count : req.signedCookies.cart_item_count,
-    user : req.signedCookies.user });
-});
-
+accountRouter.get(
+    "/",
+    authorize("Normal", "Admin"),
+    (req: Request, res: Response) => {
+        res.render("account.ejs", {
+            url: "/account",
+            cart_item_count: req.signedCookies.cart_item_count,
+            user: req.signedCookies.user,
+        });
+    },
+);
