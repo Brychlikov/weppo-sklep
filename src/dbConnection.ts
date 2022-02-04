9d2018113cf28b0aa1ae0a27b85c0890d425b0b6
brych <brychlikow@gmai.com>
2022-02-04 19:36:40 +0100

    change connection style to connectionString. requires changes to .env!

--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -6,19 +6,6 @@ dotenv.config();
 
 export const knex = knexMod({
     client: "pg",
-    connection: {
-        host: "localhost",
-        port: 5432,
-        user: process.env['PG_USER'],
-        password: process.env['PG_PASSWORD'],
-        database: process.env['PG_DB_NAME'],
-    },
-});
+    connection: process.env.DATABASE_URL });
 
-export const pool = new pg.Pool({
-    host: "localhost",
-    port: 5432,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DB_NAME,
-});
+export const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL});
